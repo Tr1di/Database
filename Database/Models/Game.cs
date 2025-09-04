dd232414c65434f27c2c2f11e7cad28650808c53
топ <@TG>
2025-09-04 21:14:30 +0300

    Добавьте файлы проекта.

---
+++ Database/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Database.Models;
+
+public class Game
+{
+    public virtual Guid Id { get; set; }
+    public virtual required string Title { get; set; }
+}
+
+public class GameMap : ClassMap<Game>
+{
+    public GameMap()
+    {
+        Id(x => x.Id).GeneratedBy.Guid();
+        Map(x => x.Title).Length(255);
+    }
+}
---
+++ Database/Program.cs
@@ -0,0 +1,27 @@
+﻿using Database.Models;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Tool.hbm2ddl;
+
+using var sessionFactory = Fluently.Configure()
+    .Database(SQLiteConfiguration.Standard
+        .ConnectionString("Data Source=Library.sqlite")
+    )
+    .Mappings(x => x.FluentMappings
+        .Add<GameMap>()
+    )
+    .ExposeConfiguration(
+        cfg => new SchemaExport(cfg)
+            .Create(false, true)
+    )
+    .BuildConfiguration()
+    .BuildSessionFactory();
+
+using var session = sessionFactory.OpenSession();
+
+session.Save(new Game
+{
+    Title = "123"
+});
+
+session.Flush();
